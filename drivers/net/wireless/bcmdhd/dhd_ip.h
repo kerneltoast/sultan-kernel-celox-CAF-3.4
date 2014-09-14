e86614f34002b77d819a4cba0ed72df0bad5dff9
Sultanxda <sultanxda@gmail.com>
2014-09-14 15:05:06 -0700

    bcmdhd: Import v1.88.64 drivers from Galaxy S5 kernel
    Signed-off-by: Sultanxda <sultanxda@gmail.com>

--- drivers/net/wireless/bcmdhd/dhd_ip.h
+++ drivers/net/wireless/bcmdhd/dhd_ip.h
@@ -1,9 +1,11 @@
 /*
- * Time stamps for latency measurements 
+ * Header file describing the common ip parser function.
  *
- * Copyright (C) 1999-2011, Broadcom Corporation
+ * Provides type definitions and function prototypes used to parse ip packet.
+ *
+ * Copyright (C) 1999-2014, Broadcom Corporation
  * 
- *         Unless you and Broadcom execute a separate written software license
+ *      Unless you and Broadcom execute a separate written software license
  * agreement governing use of this software, this software is licensed to you
  * under the terms of the GNU General Public License version 2 (the "GPL"),
  * available at http://www.broadcom.com/licenses/GPLv2.php, with the
@@ -21,54 +23,20 @@
  * software in any way with any other Broadcom software provided under a license
  * other than the GPL, without Broadcom's express prior written consent.
  *
- * $Id: htsf.h 277737 2011-08-16 17:54:59Z $
+ * $Id$
  */
-#ifndef _HTSF_H_
-#define _HTSF_H_
 
-#define HTSFMAGIC       	0xCDCDABAB  /* in network order for tcpdump  */
-#define HTSFENDMAGIC    	0xEFEFABAB  /* to distinguish from RT2 magic */
-#define HTSF_HOSTOFFSET		102
-#define HTSF_DNGLOFFSET		HTSF_HOSTOFFSET	- 4
-#define HTSF_DNGLOFFSET2	HTSF_HOSTOFFSET	+ 106
-#define HTSF_MIN_PKTLEN 	200
-#define ETHER_TYPE_BRCM_PKTDLYSTATS     0x886d
+#ifndef _dhd_ip_h_
+#define _dhd_ip_h_
 
-typedef enum htsfts_type {
-	T10,
-	T20,
-	T30,
-	T40,
-	T50,
-	T60,
-	T70,
-	T80,
-	T90,
-	TA0,
-	TE0
-} htsf_timestamp_t;
+typedef enum pkt_frag
+{
+	DHD_PKT_FRAG_NONE = 0,
+	DHD_PKT_FRAG_FIRST,
+	DHD_PKT_FRAG_CONT,
+	DHD_PKT_FRAG_LAST
+} pkt_frag_t;
 
-typedef struct {
-	uint32 magic;
-	uint32 prio;
-	uint32 seqnum;
-	uint32 misc;
-	uint32 c10;
-	uint32 t10;
-	uint32 c20;
-	uint32 t20;
-	uint32 t30;
-	uint32 t40;
-	uint32 t50;
-	uint32 t60;
-	uint32 t70;
-	uint32 t80;
-	uint32 t90;
-	uint32 cA0;
-	uint32 tA0;
-	uint32 cE0;
-	uint32 tE0;
-	uint32 endmagic;
-} htsfts_t;
+extern pkt_frag_t pkt_frag_info(osl_t *osh, void *p);
 
-#endif /* _HTSF_H_ */
+#endif /* _dhd_ip_h_ */
