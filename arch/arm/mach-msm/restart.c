122715878eb9b5ee80b21cdd5b6147a768e5bf17
Sultanxda <sultanxda@gmail.com>
2014-09-14 15:22:59 -0700

    mach-msm: restart: Add changes from Samsung
    From SGH-I727_NA_JB_Opensource.
    
    Signed-off-by: Sultanxda <sultanxda@gmail.com>

--- arch/arm/mach-msm/restart.c
+++ arch/arm/mach-msm/restart.c
@@ -26,6 +26,9 @@
 #include <linux/mfd/pm8xxx/misc.h>
 
 #include <asm/mach-types.h>
+#include <mach/sec_debug.h>  // onlyjazz
+#include <linux/notifier.h> // klaatu
+#include <linux/ftrace.h> // klaatu
 
 #include <mach/msm_iomap.h>
 #include <mach/restart.h>
@@ -45,14 +48,21 @@
 #define RESTART_REASON_ADDR 0x65C
 #define DLOAD_MODE_ADDR     0x0
 
-#define SCM_IO_DISABLE_PMIC_ARBITER	1
-
-#ifdef CONFIG_LGE_CRASH_HANDLER
-#define LGE_ERROR_HANDLER_MAGIC_NUM	0xA97F2C46
-#define LGE_ERROR_HANDLER_MAGIC_ADDR	0x18
-void *lge_error_handler_cookie_addr;
-static int ssr_magic_number = 0;
+#define RESTART_LPM_BOOT_MODE		0x77665506
+#define RESTART_ARM11FOTA_MODE          0x77665503
+#define RESTART_RECOVERY_MODE           0x77665502
+#define RESTART_OTHERBOOT_MODE          0x77665501
+#define RESTART_FASTBOOT_MODE           0x77665500
+#ifdef CONFIG_SEC_DEBUG
+#define RESTART_SECDEBUG_MODE           0x776655EE
+#define RESTART_SECCOMMDEBUG_MODE       0x7766DEAD
 #endif
+// NOT USE 0x776655FF~0x77665608 command
+#define RESTART_HOMEDOWN_MODE           0x776655FF
+#define RESTART_HOMEDOWN_MODE_END       0x77665608
+#define RESTART_CHECKSUM_FAIL_MODE      0x77667752
+
+#define SCM_IO_DISABLE_PMIC_ARBITER	1
 
 static int restart_mode;
 void *restart_reason;
@@ -64,15 +74,22 @@ static void __iomem *msm_tmr0_base;
 static int in_panic;
 static void *dload_mode_addr;
 
+#if 0	/* onlyjazz.ef24 : intentionally remove it */
 /* Download mode master kill-switch */
 static int dload_set(const char *val, struct kernel_param *kp);
 static int download_mode = 1;
 module_param_call(download_mode, dload_set, param_get_int,
 			&download_mode, 0644);
+#endif	/* onlyjazz.ef24 : intentionally remove it */
 
 static int panic_prep_restart(struct notifier_block *this,
 			      unsigned long event, void *ptr)
 {
+	if(!sec_debug_is_enabled()) {
+		printk(KERN_NOTICE "panic_prep_restart\n");
+		return NOTIFY_DONE;
+	}
+	printk(KERN_NOTICE "panic_prep_restart, in_panic = 1\n");
 	in_panic = 1;
 	return NOTIFY_DONE;
 }
@@ -87,14 +104,14 @@ static void set_dload_mode(int on)
 		__raw_writel(on ? 0xE47B337D : 0, dload_mode_addr);
 		__raw_writel(on ? 0xCE14091A : 0,
 		       dload_mode_addr + sizeof(unsigned int));
-#ifdef CONFIG_LGE_CRASH_HANDLER
-		__raw_writel(on ? LGE_ERROR_HANDLER_MAGIC_NUM : 0,
-				lge_error_handler_cookie_addr);
-#endif
 		mb();
+
+		// klaatu
+		pr_err("set_dload_mode <%d> ( %lX )\n", on, CALLER_ADDR0);
 	}
 }
 
+#if 0	/* onlyjazz.ef24 : intentionally remove it */
 static int dload_set(const char *val, struct kernel_param *kp)
 {
 	int ret;
@@ -112,12 +129,10 @@ static int dload_set(const char *val, struct kernel_param *kp)
 	}
 
 	set_dload_mode(download_mode);
-#ifdef CONFIG_LGE_CRASH_HANDLER
-	ssr_magic_number = 0;
-#endif
 
 	return 0;
 }
+#endif	/* onlyjazz.ef24 : intentionally remove it */
 #else
 #define set_dload_mode(x) do {} while (0)
 #endif
@@ -125,26 +140,25 @@ static int dload_set(const char *val, struct kernel_param *kp)
 void msm_set_restart_mode(int mode)
 {
 	restart_mode = mode;
-#ifdef CONFIG_LGE_CRASH_HANDLER
-	if (download_mode == 1 && (mode & 0xFFFF0000) == 0x6D630000)
-		panic("LGE crash handler detected panic");
-#endif
 }
 EXPORT_SYMBOL(msm_set_restart_mode);
 
 static void __msm_power_off(int lower_pshold)
 {
+	local_irq_disable();
 	printk(KERN_CRIT "Powering off the SoC\n");
 #ifdef CONFIG_MSM_DLOAD_MODE
 	set_dload_mode(0);
 #endif
 	pm8xxx_reset_pwr_off(0);
+	printk(KERN_ERR "pm8xxx_reset_pwr_off is called\n");
 
 	if (lower_pshold) {
 		__raw_writel(0, PSHOLD_CTL_SU);
 		mdelay(10000);
 		printk(KERN_ERR "Powering off has failed\n");
 	}
+	local_irq_enable();
 	return;
 }
 
@@ -195,48 +209,18 @@ static irqreturn_t resout_irq_handler(int irq, void *dev_id)
 	return IRQ_HANDLED;
 }
 
-#ifdef CONFIG_LGE_CRASH_HANDLER
-#define SUBSYS_NAME_MAX_LENGTH	40
-
-int get_ssr_magic_number(void)
-{
-	return ssr_magic_number;
-}
-
-void set_ssr_magic_number(const char* subsys_name)
-{
-	int i;
-	const char *subsys_list[] = {
-		"modem", "riva", "dsps", "lpass",
-		"external_modem", "gss",
-	};
-
-	ssr_magic_number = (0x6d630000 | 0x0000f000);
-
-	for (i=0; i < ARRAY_SIZE(subsys_list); i++) {
-		if (!strncmp(subsys_list[i], subsys_name,
-					SUBSYS_NAME_MAX_LENGTH)) {
-			ssr_magic_number = (0x6d630000 | ((i+1)<<12));
-			break;
-		}
-	}
-}
-
-void set_kernel_crash_magic_number(void)
-{
-	pet_watchdog();
-	if (ssr_magic_number == 0)
-		__raw_writel(0x6d630100, restart_reason);
-	else
-		__raw_writel(restart_mode, restart_reason);
-}
-#endif /* CONFIG_LGE_CRASH_HANDLER */
-
 void msm_restart(char mode, const char *cmd)
 {
+	int error;
 
 #ifdef CONFIG_MSM_DLOAD_MODE
 
+#ifdef CONFIG_SEC_DEBUG // klaatu
+	if( sec_debug_is_enabled() && ((restart_mode == RESTART_DLOAD) || in_panic) )
+		set_dload_mode(1);
+	else
+		set_dload_mode(0);
+#else
 	/* This looks like a normal reboot at this point. */
 	set_dload_mode(0);
 
@@ -244,17 +228,16 @@ void msm_restart(char mode, const char *cmd)
 	set_dload_mode(in_panic);
 
 	/* Write download mode flags if restart_mode says so */
-	if (restart_mode == RESTART_DLOAD) {
+	if (restart_mode == RESTART_DLOAD)
 		set_dload_mode(1);
-#ifdef CONFIG_LGE_CRASH_HANDLER
-		writel(0x6d63c421, restart_reason);
-		goto reset;
 #endif
-	}
 
+	#if 0 /* onlyjazz.ef24 : intentionally remove it */
 	/* Kill download mode if master-kill switch is set */
 	if (!download_mode)
 		set_dload_mode(0);
+	#endif /* onlyjazz.ef24 : intentionally remove it */
+
 #endif
 
 	printk(KERN_NOTICE "Going down for restart now\n");
@@ -263,25 +246,40 @@ void msm_restart(char mode, const char *cmd)
 
 	if (cmd != NULL) {
 		if (!strncmp(cmd, "bootloader", 10)) {
-			__raw_writel(0x77665500, restart_reason);
+			__raw_writel(RESTART_FASTBOOT_MODE, restart_reason);
 		} else if (!strncmp(cmd, "recovery", 8)) {
-			__raw_writel(0x77665502, restart_reason);
+			__raw_writel(RESTART_RECOVERY_MODE, restart_reason);
+		} else if (!strncmp(cmd, "download", 8)) {
+			unsigned long code=0;
+			error = strict_strtoul(cmd + 8, 16, &code);
+			code = code & 0xff;
+			__raw_writel(RESTART_HOMEDOWN_MODE + code, restart_reason);
 		} else if (!strncmp(cmd, "oem-", 4)) {
 			unsigned long code;
-			code = simple_strtoul(cmd + 4, NULL, 16) & 0xff;
+			error = strict_strtoul(cmd + 4, 16, &code);
+			code = code & 0xff;
 			__raw_writel(0x6f656d00 | code, restart_reason);
+#ifdef CONFIG_SEC_DEBUG
+		} else if (!strncmp(cmd, "sec_debug_hw_reset", 18)) {
+			__raw_writel(0x776655ee, restart_reason);
+		} else if (!strncmp(cmd, "sec_debug_comm_dump", 19)) {
+			__raw_writel(0x7766DEAD, restart_reason);
+#endif
+		} else if (!strncmp(cmd, "arm11_fota", 10)) {
+			__raw_writel(RESTART_ARM11FOTA_MODE, restart_reason);
+		} else if (!strncmp(cmd, "checksum_fail", 13)) {
+			__raw_writel(RESTART_CHECKSUM_FAIL_MODE, restart_reason);
 		} else {
-			__raw_writel(0x77665501, restart_reason);
+			__raw_writel(RESTART_OTHERBOOT_MODE, restart_reason);
 		}
-	} else {
-		__raw_writel(0x77665501, restart_reason);
 	}
-#ifdef CONFIG_LGE_CRASH_HANDLER
-	if (in_panic == 1)
-		set_kernel_crash_magic_number();
-reset:
-#endif /* CONFIG_LGE_CRASH_HANDLER */
+#ifdef CONFIG_SEC_DEBUG
+	else {
+		writel(0x12345678, restart_reason);    /* clear abnormal reset flag */
+	}
+#endif
 
+#if 0 /* onlyjazz.el20 : it was commented out in celox gingerbread, according to the request from waro.park */
 	__raw_writel(0, msm_tmr0_base + WDT0_EN);
 	if (!(machine_is_msm8x60_fusion() || machine_is_msm8x60_fusn_ffa())) {
 		mb();
@@ -289,6 +287,7 @@ reset:
 		mdelay(5000);
 		pr_notice("PS_HOLD didn't work, falling back to watchdog\n");
 	}
+#endif
 
 	__raw_writel(1, msm_tmr0_base + WDT0_RST);
 	__raw_writel(5*0x31F3, msm_tmr0_base + WDT0_BARK_TIME);
@@ -313,10 +312,6 @@ static int __init msm_pmic_restart_init(void)
 		pr_warn("no pmic restart interrupt specified\n");
 	}
 
-#ifdef CONFIG_LGE_CRASH_HANDLER
-	__raw_writel(0x6d63ad00, restart_reason);
-#endif
-
 	return 0;
 }
 
@@ -327,11 +322,12 @@ static int __init msm_restart_init(void)
 #ifdef CONFIG_MSM_DLOAD_MODE
 	atomic_notifier_chain_register(&panic_notifier_list, &panic_blk);
 	dload_mode_addr = MSM_IMEM_BASE + DLOAD_MODE_ADDR;
-#ifdef CONFIG_LGE_CRASH_HANDLER
-	lge_error_handler_cookie_addr = MSM_IMEM_BASE +
-		LGE_ERROR_HANDLER_MAGIC_ADDR;
-#endif
-	set_dload_mode(download_mode);
+
+	/* Reset detection is switched on below.*/
+	if( sec_debug_is_enabled() )
+		set_dload_mode(1);
+	else
+		set_dload_mode(0);
 #endif
 	msm_tmr0_base = msm_timer_get_timer0_base();
 	restart_reason = MSM_IMEM_BASE + RESTART_REASON_ADDR;
